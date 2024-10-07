7e38ac34e083bf6b1dd9b8636c04265ba3324e33
Alexandr <shelter.play.akk@mail.ru>
2024-10-07 23:09:04 +0700

    new file:   .vscode/settings.json 	modified:   pom.xml 	modified:   src/main/java/com/example/App.java 	modified:   target/classes/com/example/App.class

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 import java.util.function.Function;
 
 import javax.swing.*;
+
 import java.awt.*;
+// import java.awt.event.KeyListener;
+// import java.awt.event.KeyEvent;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -16,19 +19,19 @@ import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static XYSeries series;
     public static JFreeChart chart;
     public static ChartPanel chartPanel;
+    public static 
+    Function<Double, Double> func = x -> Math.pow(x, 2) - 4; // setting the equation 
 
     public static double method(Function<Double, Double> func, double leftPoint, double rightPoint, double accuracy, int[] count){
         double funcLeftPoint = func.apply(leftPoint); //calculating value at the left point
         double funcRightPoint = func.apply(rightPoint); // calculating value at the right point
+        double secondDer = secondDerivative(leftPoint);
 
 
         if(funcLeftPoint*funcRightPoint >= 0){ // checking the existance of a root of an equation
@@ -37,13 +40,11 @@ public class App
 
         double newPoint  = 0;
 
-        if(funcLeftPoint>=0){ // graph type check
+        if(funcLeftPoint*secondDer>0){ // graph type check
             while(true){
                 newPoint = rightPoint - (funcRightPoint*(rightPoint - leftPoint))/(funcRightPoint - funcLeftPoint); // using the formula for a new point
                 double funcNewPoint = func.apply(newPoint); // calculating value at the new point
-                XYPlot plot = (XYPlot) chart.getPlot();
-                XYLineAnnotation line = new XYLineAnnotation(leftPoint, func.apply(leftPoint), newPoint, func.apply(newPoint), new BasicStroke(1.0f), Color.RED);
-                plot.addAnnotation(line);
+                drawLine(leftPoint, func.apply(leftPoint), newPoint, funcNewPoint);
 
                 count[0]++;
 
@@ -58,9 +59,7 @@ public class App
             while(true){
                 newPoint = leftPoint - (funcLeftPoint*(rightPoint - leftPoint))/(funcRightPoint-funcLeftPoint);
                 double funcNewPoint = func.apply(newPoint);
-                XYPlot plot = (XYPlot) chart.getPlot();
-                XYLineAnnotation line = new XYLineAnnotation(rightPoint, func.apply(rightPoint), newPoint, func.apply(newPoint), new BasicStroke(1.0f), Color.RED);
-                plot.addAnnotation(line);
+                drawLine(rightPoint, func.apply(rightPoint), newPoint, funcNewPoint);
 
                 count[0]++;
 
@@ -74,9 +73,33 @@ public class App
         }
     }
 
+    public static void drawLine(double x1, double y1, double x2, double y2){
+        try {
+            XYPlot plot = (XYPlot) chart.getPlot();
+            XYLineAnnotation line = new XYLineAnnotation(x1, y1, x2, y2, new BasicStroke(0.3f), Color.RED);
+            plot.addAnnotation(line);
+
+            Thread.sleep(500);//delay for rendering graph
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Первая производная f'(x)
+    public static double firstDerivative(double x) {
+        double h = 1e-5; // Маленькое значение для численного дифференцирования
+        return (func.apply(x + h) - func.apply(x - h)) / (2 * h); // Центральная разностная схема
+    }
+
+    // Вторая производная f''(x)
+    public static double secondDerivative(double x) {
+        double h = 1e-5; // Маленькое значение для численного дифференцирования
+        return (firstDerivative(x + h) - firstDerivative(x - h)) / (2 * h); // Центральная разностная схема
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Function<Double, Double> func = x -> Math.pow(x, 2) - 4; // setting the equation 
         int[] count = {0};
         System.out.println("Enter leftPoint, rightPoint and accuracy: ");
         int leftPoint = scan.nextInt();
@@ -87,7 +110,7 @@ public class App
 
         // Добавляем данные в серию (например, от 0 до 10)
         for (double x = leftPoint; x <= rightPoint; x += 0.1) {
-            double y = func.apply(x); // Замените на вашу функцию
+            double y = func.apply(x);
             series.add(x, y);
         }
 
@@ -108,13 +131,14 @@ public class App
 
         // Создаем панель для отображения графика
         chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
+        chartPanel.setPreferredSize(new java.awt.Dimension(1000, 800));
 
         // Создаем окно и добавляем панель с графиком
         JFrame frame = new JFrame("График функции");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(chartPanel);
         frame.pack();
+        frame.setAlwaysOnTop(true);
         frame.setVisible(true);
 
 
