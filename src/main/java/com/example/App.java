d4229a1dd9580ea63af3a02faae2f8659143e268
Alexandr Kovalchuk <shelter.play.akk@mail.ru>
2024-10-14 17:17:00 +0700

    modified:   src/main/java/com/example/App.java 	modified:   target/classes/com/example/App$1.class 	modified:   target/classes/com/example/App.class

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,13 @@
 package com.example;
 
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Function;
 import java.util.List;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+
 import java.awt.*;
 
 import org.jfree.chart.ChartFactory;
@@ -25,23 +28,27 @@ public class App
     public static ChartPanel chartPanel;
     public static Function<Double, Double> func = x -> Math.pow(x, 2) - 4; // setting the equation 
     public static int count=0;
+    public static JLabel rootLabel;
+    public static JLabel valueRootLabel;
+    public static JLabel countLabel;
 
-    // Прописываем метод хорд
+    // Chord method
     public static void method(Function<Double, Double> func, double leftPoint, double rightPoint, double accuracy){
         // Using swing utility for assinc compliting the chord method 
         // (have to be because it locks the GUI and does not show the method work with the graph)
-        SwingWorker<Void, Double[]> worker = new SwingWorker<Void,Double[]>() {
+        SwingWorker<Double, Double[]> worker = new SwingWorker<Double,Double[]>() {
             double localLeftPoint = leftPoint;
             double localRightPoint = rightPoint;
             @Override
-            protected Void doInBackground() throws Exception{
+            protected Double doInBackground() throws Exception{
                 double funcLeftPoint = func.apply(localLeftPoint); //calculating value at the left point
                 double funcRightPoint = func.apply(localRightPoint); // calculating value at the right point
                 double secondDer = secondDerivative(localLeftPoint); // calculating the secon derivative
 
 
                 if(funcLeftPoint*funcRightPoint >= 0){ // checking the existance of a root of an equation
-                    throw new IllegalArgumentException("Знаки правой и левой границы должны быть различны!");
+                    JOptionPane.showMessageDialog(null, "Move the graph boundaries!", "Error: The root does not exists", JOptionPane.ERROR_MESSAGE);
+                    System.exit(1);
                 }
 
                 double newPoint  = 0;
@@ -58,7 +65,7 @@ public class App
 
                         // root check
                         if(Math.abs(localRightPoint-newPoint)<accuracy){
-                            return null;
+                            return newPoint;
                         }
 
                         // mooving the boundary of the graph
@@ -69,14 +76,14 @@ public class App
                     while(true){
                         newPoint = localLeftPoint - (funcLeftPoint*(localRightPoint - localLeftPoint))/(funcRightPoint-funcLeftPoint);
                         double funcNewPoint = func.apply(newPoint);
-                        publish(new Double[]{localRightPoint, func.apply(localRightPoint), newPoint, funcNewPoint});
+                        publish(new Double[]{localRightPoint, func.apply(localRightPoint), newPoint, funcNewPoint, newPoint});
 
                         Thread.sleep(500);
 
                         count++;
 
                         if(Math.abs(newPoint - localLeftPoint)<accuracy){
-                            return null;
+                            return newPoint;
                         }
 
                         localLeftPoint = newPoint;
@@ -91,6 +98,23 @@ public class App
                     XYPlot plot = (XYPlot) chart.getPlot();
                     XYLineAnnotation line = new XYLineAnnotation(el[0], el[1], el[2], el[3], new BasicStroke(0.3f), Color.RED);
                     plot.addAnnotation(line);
+                    rootLabel.setText("The temporary root is: " + el[4]);
+                }
+            }
+
+            @Override
+            protected void done(){
+                try {
+                    double root = get();
+                    rootLabel.setText("The root at the x = " + root);
+                    valueRootLabel.setText("At this point the function takes the value = " + func.apply(root));
+                    countLabel.setText("Achived in " + count + " iterations");
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
                 }
             }
         };
@@ -109,7 +133,7 @@ public class App
         return (firstDerivative(x + h) - firstDerivative(x - h)) / (2 * h); // Центральная разностная схема
     }
 
-    // Создаем второе окно для графика и метода
+    // Creating the second window for graph and method
     public static void showGraphWMethod(double leftPoint, double rightPoint, double accuracy){
         // Создаем серию данных
         series = new XYSeries("Chord method");
@@ -137,7 +161,7 @@ public class App
 
         // Создаем панель для отображения графика
         chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(1000, 800));
+        chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
 
         // Создаем кнопку для вызова метода
         JButton butt = new JButton("Use method");
@@ -152,47 +176,109 @@ public class App
         frame.getContentPane().add(chartPanel, BorderLayout.CENTER);
         frame.getContentPane().add(butt, BorderLayout.SOUTH);
         frame.pack();
-        frame.setAlwaysOnTop(true);
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter leftPoint, rightPoint and accuracy: ");
-        int leftPoint = scan.nextInt();
-        int rightPoint = scan.nextInt();
-        double accuracy = scan.nextDouble();
 
+        //Setting the frame
         JFrame frame = new JFrame("Function selection");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 400);
-        frame.setLayout(new FlowLayout());
+        frame.setSize(430, 250);
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER));
 
+        // Creating the methods buttons
         JRadioButton firstGraphButt = new JRadioButton("Graph x^2 - 4");
         JRadioButton secondGraphButt = new JRadioButton("Graph -x^3 + x^2 - 5");
-        JButton showGraphButt = new JButton("Show the choosen function graph");
 
+        double[] data = new double[3];
+        JTextField[] textFields = new JTextField[3];
+        // Creating text fields for data
+        for(int i=0; i<3; i++){
+            textFields[i] = new JTextField(8);
+        }
+
+        // Creating the data senging button
+        JButton sendDataButt = new JButton("Send the data");
+        sendDataButt.setSize(50, 20);
+
+        // Creating the create graph button
+        JButton showGraphButt = new JButton("Show the choosen function graph");
+        showGraphButt.setSize(50, 20);
+
+        // Collecting the methods buttons intonthe radio buttons group
         ButtonGroup graphsButtonGroup = new ButtonGroup();
         graphsButtonGroup.add(firstGraphButt);
         graphsButtonGroup.add(secondGraphButt);
 
         firstGraphButt.setSelected(true);
 
-        frame.add(firstGraphButt);
-        frame.add(secondGraphButt);
-        frame.add(showGraphButt);
+        JPanel radioButtonsPanel = new JPanel();
+        radioButtonsPanel.add(firstGraphButt);
+        radioButtonsPanel.add(secondGraphButt);
 
-        showGraphButt.addActionListener(e -> {
-            func = firstGraphButt.isSelected() ? x -> Math.pow(x, 2) - 4 : x -> -Math.pow(x, 3) + Math.pow(x, 2) - 5;
-            showGraphWMethod(leftPoint, rightPoint, accuracy);
+        JPanel buttonsPanel  = new JPanel();
+        buttonsPanel.setLayout(new FlowLayout());
+        buttonsPanel.add(sendDataButt);
+        buttonsPanel.add(showGraphButt);
+
+        // Creating labels for results
+        rootLabel = new JLabel("Root is being found...");
+        valueRootLabel = new JLabel();
+        countLabel = new JLabel();
+
+        JPanel labelPanel = new JPanel();
+        labelPanel.setLayout(new GridLayout(3, 1));
+        labelPanel.setPreferredSize(new Dimension(400, 60));
+        labelPanel.add(rootLabel);
+        labelPanel.add(valueRootLabel);
+        labelPanel.add(countLabel);
+        labelPanel.setBackground(Color.WHITE);
+
+        // creating border and padding for label panel
+        Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
+        Border padding = BorderFactory.createEmptyBorder(4, 10, 10, 4);
+        Border compoundBorder = BorderFactory.createCompoundBorder(border, padding);
+        labelPanel.setBorder(compoundBorder);
+
+        JPanel fieldsPanel = new JPanel();
+        for(JTextField field : textFields){
+            fieldsPanel.add(field);
+        }
+
+        // Adding all of the buttons and text fields into the frame
+        frame.add(radioButtonsPanel);
+        frame.add(fieldsPanel);
+        frame.add(buttonsPanel);
+        frame.add(labelPanel);
+        // disable graph button because we have no data for it
+        showGraphButt.setEnabled(false);
+
+        // sending the data for graph and method and validate it
+        sendDataButt.addActionListener(e -> {
+            for(int i=0; i<3; i++){
+                String input = textFields[i].getText();
+                try {
+                    data[i] = Double.parseDouble(input);
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Error: '" + input + 
+                    "' is not the number!", "Input error", JOptionPane.ERROR_MESSAGE);
+                    System.exit(1);
+                }
+            }
+            showGraphButt.setEnabled(true);;
         });
 
+        // drawing the graph
+        showGraphButt.addActionListener(e -> {
+            func = firstGraphButt.isSelected() ? x -> Math.pow(x, 2) - 4 : x -> -Math.pow(x, 3) + Math.pow(x, 2) - 5;
+            showGraphWMethod(data[0], data[1], data[2]);
+        });
+
+        // Set frame to visible
         frame.setVisible(true);
-        frame.setAlwaysOnTop(true);
-        
-        // System.out.println("The root at the x = " + valueAtTheRoot);
-        // System.out.println("At this point the function takes the value = " + func.apply(valueAtTheRoot));
-        // System.out.println("Achived in " + count[0] + " iterations");
+
         scan.close();
     }
 }
