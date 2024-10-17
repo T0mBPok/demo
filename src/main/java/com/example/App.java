01d77d3363c82e31acd974abb9353e66d630f9b9
Alexandr Kovalchuk <shelter.play.akk@mail.ru>
2024-10-17 19:46:18 +0700

    modified:   src/main/java/com/example/App.java 	modified:   target/classes/com/example/App$1.class 	modified:   target/classes/com/example/App.class

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -13,6 +13,7 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.annotations.XYLineAnnotation;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.xy.XYDataset;
@@ -24,15 +25,21 @@ public class App
 {
     public static Function<Double, Double> func; // setting the equation 
     public static XYSeries series;
+    public static XYDataset dataset;
     public static JFreeChart chart;
     public static ChartPanel chartPanel;
+    public static JFrame frame;
     public static int count=0;
     public static JLabel rootLabel;
     public static JLabel valueRootLabel;
     public static JLabel countLabel;
+    public static double leftPoint = 0;
+    public static double rightPoint = 4;
+    public static double accuracy = .0001;
+    public static double[] data = new double[]{leftPoint, rightPoint, accuracy};
 
     // Chord method
-    public static void method(Function<Double, Double> func, double leftPoint, double rightPoint, double accuracy){
+    public static void method(Function<Double, Double> func){
         // Using swing utility for assinc compliting the chord method 
         // (have to be because it locks the GUI and does not show the method work with the graph)
         SwingWorker<Double, Double[]> worker = new SwingWorker<Double,Double[]>() {
@@ -47,7 +54,6 @@ public class App
 
                 if(funcLeftPoint*funcRightPoint > 0){ // checking the existance of a root of an equation
                     JOptionPane.showMessageDialog(null, "Move the graph boundaries!", "Error: The root does not exists", JOptionPane.ERROR_MESSAGE);
-                    System.exit(1);
                 }
 
                 double newPoint  = 0;
@@ -56,7 +62,7 @@ public class App
                     while(true){
                         newPoint = localRightPoint - (funcRightPoint*(localRightPoint - localLeftPoint))/(funcRightPoint - funcLeftPoint); // using the formula for a new point
                         double funcNewPoint = func.apply(newPoint); // calculating value at the new point
-                        publish(new Double[]{localLeftPoint, func.apply(localLeftPoint), newPoint, funcNewPoint, newPoint}); // sending the data into process to drow the line
+                        publish(new Double[]{localLeftPoint, func.apply(localLeftPoint), newPoint, funcNewPoint}); // sending the data into process to drow the line
 
                         Thread.sleep(500);//delay for rendering graph
 
@@ -75,7 +81,7 @@ public class App
                     while(true){
                         newPoint = localLeftPoint - (funcLeftPoint*(localRightPoint - localLeftPoint))/(funcRightPoint-funcLeftPoint);
                         double funcNewPoint = func.apply(newPoint);
-                        publish(new Double[]{localRightPoint, func.apply(localRightPoint), newPoint, funcNewPoint, newPoint});
+                        publish(new Double[]{localRightPoint, func.apply(localRightPoint), newPoint, funcNewPoint});
 
                         Thread.sleep(500);
 
@@ -95,9 +101,11 @@ public class App
             protected void process(List<Double[]> chunks){
                 for(Double[] el : chunks){
                     XYPlot plot = (XYPlot) chart.getPlot();
-                    XYLineAnnotation line = new XYLineAnnotation(el[0], el[1], el[2], el[3], new BasicStroke(0.3f), Color.RED);
-                    plot.addAnnotation(line);
-                    rootLabel.setText("The temporary root is: " + el[4]);
+                    XYLineAnnotation line1 = new XYLineAnnotation(el[2], func.apply(el[2]), el[2], 0, new BasicStroke(0.5f), Color.RED);
+                    XYLineAnnotation line2 = new XYLineAnnotation(el[0], el[1], el[2], el[3], new BasicStroke(0.5f), Color.RED);
+                    plot.addAnnotation(line1);
+                    plot.addAnnotation(line2);
+                    rootLabel.setText("The temporary root is: " + el[2]);
                 }
             }
 
@@ -132,21 +140,27 @@ public class App
 
     // Creating the second window for graph and method
     public static void showGraphWMethod(double leftPoint, double rightPoint, double accuracy){
-        // Создаем серию данных
-        series = new XYSeries("Chord method");
-
         // Добавляем точки графика 
         for (double x = leftPoint; x <= rightPoint; x += 0.1) {
             double y = func.apply(x);
             series.add(x, y);
         }
+        
+        XYPlot plot = (XYPlot) chart.getPlot();
+        XYLineAnnotation line = new XYLineAnnotation(leftPoint, 0, rightPoint, 0, new BasicStroke(1), Color.BLACK);
+        plot.addAnnotation(line);
+    }
+
+    public static void createGraphPanel(){
+        // Создаем серию данных
+        series = new XYSeries("Equation graph");
 
         // Создаем набор данных
-        XYDataset dataset = new XYSeriesCollection(series);
+        dataset = new XYSeriesCollection(series);
 
         // Создаем график
         chart = ChartFactory.createXYLineChart(
-                "График функции", // Заголовок
+                "Equation graph",             // Заголовок
                 "x",                          // Ось X
                 "y",                          // Ось Y
                 dataset,                      // Набор данных
@@ -156,42 +170,44 @@ public class App
                 false                         // URL-адреса
         );
 
+        XYPlot plot = chart.getXYPlot();
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesPaint(0, Color.BLACK);
+        renderer.setSeriesShapesVisible(0, false);
+        plot.setRenderer(renderer);
+
         // Создаем панель для отображения графика
         chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
-
-        // Создаем кнопку для вызова метода
-        JButton butt = new JButton("Use method");
-        // Добавляем на нее обработчик событий
-        butt.addActionListener(e -> {
-            method(func, leftPoint, rightPoint, accuracy);
-        });
+        chartPanel.setPreferredSize(new java.awt.Dimension(1100, 770));
 
         // Создаем окно и добавляем панель с графиком и кнопку
-        JFrame frame = new JFrame("График функции");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(chartPanel, BorderLayout.CENTER);
-        frame.getContentPane().add(butt, BorderLayout.SOUTH);
-        frame.pack();
-        frame.setVisible(true);
+        JPanel panel = new JPanel();
+        panel.add(chartPanel, BorderLayout.CENTER);
+        frame.add(panel);
     }
 
     public static void main(String[] args) {
         //Setting the frame
-        JFrame frame = new JFrame("Function selection");
+        frame = new JFrame("Find the root");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(430, 250);
-        frame.setLayout(new FlowLayout(FlowLayout.CENTER));
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setLayout(new FlowLayout());
+
+        // Info panel
+        JPanel greatPanel = new JPanel();
+        greatPanel.setLayout(new GridLayout(6, 1));
 
         // Creating the methods buttons
-        JRadioButton firstGraphButt = new JRadioButton("Graph x^2 - 4"); // [-4, 0] && [0, 5] 
-        JRadioButton secondGraphButt = new JRadioButton("Graph x^3 + x^2"); // [-1.5. -0.5] && [-0.5, 0.5]
+        JRadioButton firstGraphButt = new JRadioButton("Equation y = x^2 - 4"); // [-4, 0] && [0, 5] 
+        JRadioButton secondGraphButt = new JRadioButton("Equation y =  x^3 + x^2"); // [-1.5. -0.5] && [-0.5, 0.5]
 
-        double[] data = new double[3];
         JTextField[] textFields = new JTextField[3];
+        JLabel[] labelFields = new JLabel[3];
+        String[] fieldsLabels = {"Left graph side:", "Right graph side:", "Accuracy:"};
         // Creating text fields for data
         for(int i=0; i<3; i++){
-            textFields[i] = new JTextField(8);
+            textFields[i] = new JTextField(Double.toString(data[i]), 8);
+            labelFields[i] = new JLabel(fieldsLabels[i]);
         }
 
         // Creating the data senging button
@@ -199,7 +215,7 @@ public class App
         sendDataButt.setSize(50, 20);
 
         // Creating the create graph button
-        JButton showGraphButt = new JButton("Show the choosen function graph");
+        JButton showGraphButt = new JButton("Show the choosen equation graph");
         showGraphButt.setSize(50, 20);
 
         // Collecting the methods buttons intonthe radio buttons group
@@ -210,8 +226,16 @@ public class App
         firstGraphButt.setSelected(true);
 
         JPanel radioButtonsPanel = new JPanel();
-        radioButtonsPanel.add(firstGraphButt);
-        radioButtonsPanel.add(secondGraphButt);
+        radioButtonsPanel.setLayout(new GridLayout(2,1));
+        JLabel eqnsLabel = new JLabel("Select a function to find its root!");
+        eqnsLabel.setFont(new Font("Arial",Font.PLAIN, 20));
+        JPanel n1Panel = new JPanel();
+        JPanel n2Panel = new JPanel();
+        n1Panel.add(eqnsLabel);
+        n2Panel.add(firstGraphButt);
+        n2Panel.add(secondGraphButt);
+        radioButtonsPanel.add(n1Panel);
+        radioButtonsPanel.add(n2Panel);
 
         JPanel buttonsPanel  = new JPanel();
         buttonsPanel.setLayout(new FlowLayout());
@@ -238,20 +262,45 @@ public class App
         labelPanel.setBorder(compoundBorder);
 
         JPanel fieldsPanel = new JPanel();
-        for(JTextField field : textFields){
-            fieldsPanel.add(field);
-        }
+        for(int i=0; i<3; i++){
+            JPanel tempPanel = new JPanel();
+            tempPanel.setLayout(new GridLayout(2, 1));;
+            tempPanel.add(labelFields[i]);
+            tempPanel.add(textFields[i]);
+            fieldsPanel.add(tempPanel);
+        }        
+
+        // Создаем кнопку для вызова метода
+        JButton methodButt = new JButton("Use the chord method");
+        methodButt.setSize(50, 20);
+        // Добавляем на нее обработчик событий
+        methodButt.addActionListener(e -> {
+            valueRootLabel.setText(null);
+            countLabel.setText(null);
+            method(func);
+        });
+        JPanel methodPanel = new JPanel();
+        methodPanel.add(methodButt);
 
         // Adding all of the buttons and text fields into the frame
-        frame.add(radioButtonsPanel);
-        frame.add(fieldsPanel);
-        frame.add(buttonsPanel);
-        frame.add(labelPanel);
+        greatPanel.add(radioButtonsPanel);
+        greatPanel.add(fieldsPanel);
+        greatPanel.add(buttonsPanel);
+        greatPanel.add(labelPanel);
+        greatPanel.add(methodPanel);
+        frame.add(greatPanel);
         // disable graph button because we have no data for it
         showGraphButt.setEnabled(false);
+        methodButt.setEnabled(false);
+
 
         // sending the data for graph and method and validate it
         sendDataButt.addActionListener(e -> {
+            series.clear();
+            XYPlot plot = (XYPlot) chart.getPlot();
+            plot.getAnnotations().clear();
+            chart.fireChartChanged();
+            methodButt.setEnabled(false);
             for(int i=0; i<3; i++){
                 String input = textFields[i].getText();
                 try {
@@ -262,13 +311,20 @@ public class App
                     System.exit(1);
                 }
             }
-            showGraphButt.setEnabled(true);;
+            leftPoint = data[0];
+            rightPoint = data[1];
+            accuracy = data[2];
+            showGraphButt.setEnabled(true);
         });
+
+        // Creating a panel for equation graph
+        createGraphPanel();
 
         // drawing the graph
         showGraphButt.addActionListener(e -> {
             func = firstGraphButt.isSelected() ? x -> Math.pow(x, 2) - 4 : x -> Math.pow(x, 3) + Math.pow(x, 2);
-            showGraphWMethod(data[0], data[1], data[2]);
+            showGraphWMethod(leftPoint, rightPoint, accuracy);
+            methodButt.setEnabled(true);
         });
 
         // Set frame to visible
